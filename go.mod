aefb1af59c282232f30688def5c0d0a3ca2d35e4
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-03-18 19:34:52 +0530

    lesson-03

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/mrityunjaygr8/jei-block
 
 go 1.16
+
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	golang.org/x/sys v0.0.0-20210317225723-c4fcb01b228e // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main() {
 	bc := NewBlockchain()
-	bc.AddBlock("Recieved 1BTC from god")
-	bc.AddBlock("Send 1BTC to mgr8")
+	defer bc.db.Close()
 
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	cli := CLI{bc}
+	cli.Run()
 }
